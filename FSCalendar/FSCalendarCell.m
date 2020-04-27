0acc542bd7903ad1eb0448f360eef9b9291e4450
Daniel Teixeira <daniel.teixeira@prodoctor.net>
2020-04-27 16:48:14 -0300

    Ajustado o tamanho do circulo em relação ao aumento das fontes (com acessibilidade) refs #26394

--- FSCalendar/FSCalendarCell.m
+++ FSCalendar/FSCalendarCell.m
@@ -135,7 +135,7 @@
     
     
     CGFloat titleHeight = self.bounds.size.height*5.0/6.0;
-    CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width);
+    CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width) + (_titleLabel.font.pointSize - 14);
     diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
     _shapeLayer.frame = CGRectMake((self.bounds.size.width-diameter)/2,
                                    (titleHeight-diameter)/2,
